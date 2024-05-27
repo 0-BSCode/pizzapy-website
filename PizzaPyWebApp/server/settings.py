e8c6086770c675011dff3021facce7bb8e4c641f
0-BSCode <bryanipsanchez@gmail.com>
2024-05-27 20:28:08 +0800

    Edited static file collection settings

--- PizzaPyWebApp/server/settings.py
+++ PizzaPyWebApp/server/settings.py
@@ -119,13 +119,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+print(BASE_DIR)
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static", "staticfiles_build")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
 
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    # os.path.join(BASE_DIR, "static"),
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
